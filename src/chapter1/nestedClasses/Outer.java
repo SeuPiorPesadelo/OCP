fdcd72f5bb5be94af75ba4bbbfb97421c1a53fcb
lucas <lucasdec88@gmail.com>
2018-02-22 22:41:45 -0300

    Chapter 1 - Member Inner Class e Local Inner Class

---
+++ src/chapter1/nestedClasses/Outer.java
@@ -0,0 +1,34 @@
+package chapter1.nestedClasses;
+
+/**
+ * Member Inner Class
+ * 
+ * @author Lucas
+ *
+ */
+public class Outer {
+
+	private String variable = "Oi";
+
+	//classe membro
+	protected class Inner {
+		private int repete = 3;
+
+		public void print() {
+			for (int i = 0; i < repete; i++) {
+				//pode usar variaveis da class externa
+				System.out.println(variable);
+			}
+		}
+	}
+	
+	public void callInner(){
+		Inner inner = new Inner();
+		inner.print();
+	}
+	
+	public static void main(String[] args){
+		new Outer().callInner();
+		new Outer().new Inner().print();
+	}
+}
---
+++ src/chapter1/nestedClasses/Outer1.java
@@ -0,0 +1,41 @@
+package chapter1.nestedClasses;
+
+/**
+ * Local Inner Class
+ * 
+ * @author Lucas
+ *
+ */
+public class Outer1 {
+
+	private int length = 5;
+
+	// Classe Local eh a classe q existe dentro de metodos
+	public Integer calculate() {
+		int width = 5;
+		length++;
+		
+		class Inner {// sem modificador de acesso e sem nada static
+			public int multiply() {
+				metodoDaOuterClass();// pode acessar campos/metodos da Outer
+				// so usa variaveis efetivamente finais do metodo em q a classe
+				// se encontra
+				return length * width;
+			}
+		}
+
+		Inner i = new Inner();
+		return i.multiply();
+	}
+
+	public static void metodoDaOuterClass() {
+		System.out.println("Class Outer1");
+	}
+
+	public static void main(String[] args) {
+		Outer1 out = new Outer1();
+		System.out.println(out.calculate());
+		int i = out.calculate();
+		System.out.println(i);
+	}
+}
