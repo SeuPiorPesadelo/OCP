d414dd645f4add1db8c930d48012847a78a3223a
lucas <lucasdec88@gmail.com>
2018-03-27 01:03:26 -0300

    Chapter 4 - Collectors em Streams

--- src/chapter4/stream/CollecttorsStream.java
+++ src/chapter4/stream/CollecttorsStream.java
@@ -1,5 +1,6 @@
 package chapter4.stream;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -10,6 +11,7 @@ public class CollecttorsStream {
 	public static void main(String[] args) {
 		usoBasico();
 		collectorsComMap();
+		collectorsComGroupingBy();
 	}
 
 	private static void usoBasico() {
@@ -28,8 +30,16 @@ public class CollecttorsStream {
 		System.out.println(map);
 
 		Stream<String> s1 = Stream.of("Tigre", "Elefante", "Urso", "Urubu");
-		Map<Integer, String> map1 = s1  //o q será a chave,chave aqui, BinaryOperator mergiando caso exista mais de 1 na mesma key
+		Map<Integer, String> map1 = s1 // o q será a chave,chave aqui,
+										// BinaryOperator mergiando caso exista
+										// mais de 1 na mesma key
 				.collect(Collectors.toMap(String::length, key -> key, (value1, value2) -> value1 + ", " + value2));
 		System.out.println(map1);
+	}
+
+	private static void collectorsComGroupingBy() {
+		Stream<String> s = Stream.of("Tigre", "Elefante", "Urso", "Urubu");
+		Map<Integer, List<String>> map = s.collect(Collectors.groupingBy(String::length));
+		map.get(5).forEach(p -> System.out.print(p));// TigreUrubu
 	}
 }
